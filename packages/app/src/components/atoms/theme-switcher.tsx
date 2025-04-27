d2ca2b7df6cc5131a72f0ce6410deb56af5e6b3e
andreaemmanuele <andreaemmanuele98@gmail.com>
2025-04-27 09:18:23 +0200

    feat: glow movement effect

--- packages/app/src/components/atoms/theme-switcher.tsx
+++ packages/app/src/components/atoms/theme-switcher.tsx
@@ -1,4 +1,5 @@
-import type { FC } from 'react'
+import type { FC, MouseEvent } from 'react'
+import { useState } from 'react'
 import { useFetcher } from '@remix-run/react'
 import { Button } from '@/components/ui/button'
 import { Moon } from '@/components/atoms/moon'
@@ -9,6 +10,7 @@ type ThemeSwitcherProps = {
 
 export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ current }) => {
   const fetcher = useFetcher()
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
   const toggleTheme = () => {
     const formData = new FormData()
@@ -19,13 +21,31 @@ export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ current }) => {
     })
   }
 
+  const handleMouseMove = (e: MouseEvent) => {
+    console.log(e.clientY, e.clientY, window.innerWidth, window.innerHeight)
+    setPosition((prev) => ({
+      ...prev,
+      x: Math.min(window.innerWidth - e.clientX, 15),
+      y: Math.min(e.clientY, 15),
+    }))
+  }
+
+  const handleMouseLeave = (e: MouseEvent) => {
+    setPosition({ x: 0, y: 0 })
+  }
+
   return (
     <Button
       className="relative text-black group dark:text-white bg-transparent shadow-none hover:bg-transparent"
       onClick={toggleTheme}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
     >
       <Moon active={current === 'dark'} />
-      <div className="absolute top-0 w-8 h-8 bg-lime-100 blur-2xl -z-10" />
+      <div
+        style={{ transform: `translate(-${position.x}px, ${position.y}px)` }}
+        className="absolute top-0 right-0 w-8 h-8 bg-lime-100 blur-xl -z-10 transition-transform duration-100 ease-linear"
+      />
     </Button>
   )
 }
