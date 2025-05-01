884c20d5024c4e4bbca471ceb9a9a25648a99cf1
andreaemmanuele <andreaemmanuele98@gmail.com>
2025-05-01 11:22:02 +0200

    fix: deployment

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -3,6 +3,7 @@ import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
@@ -15,4 +16,9 @@ export default defineConfig({
     tsconfigPaths(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@morpheus/shared': resolve(__dirname, '../shared/src'),
+    },
+  },
 })
