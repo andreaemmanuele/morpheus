584472e298d2c8a7a9b851062e3c48dc8df0c7f1
andreaemmanuele <andreaemmanuele98@gmail.com>
2025-05-18 11:49:22 +0200

    fix: can't delete owner from a project

--- packages/shared/src/queries/projects/delete-member.ts
+++ packages/shared/src/queries/projects/delete-member.ts
@@ -1,1 +1,1 @@
-export default `DELETE FROM projects_users_roles WHERE project_id = $1 AND user_id = $2`
+export default `DELETE FROM projects_users_roles WHERE project_id = $1 AND user_id = $2 AND role_id != 1`
