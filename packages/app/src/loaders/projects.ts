95d806bae881bbb9ceae39584c25684699405dcb
andreaemmanuele <andreaemmanuele98@gmail.com>
2025-05-18 17:52:07 +0200

    fix: invite members flow

--- packages/app/src/loaders/projects.ts
+++ packages/app/src/loaders/projects.ts
@@ -1,12 +1,24 @@
 import type { LoaderFunctionArgs } from '@remix-run/server-runtime'
 import { redirect } from '@remix-run/server-runtime'
-import { authCookie, projectCookie } from '@/cookies.server'
-import { getProjectTeamMembers } from '@/lib/projects'
+import { authCookie, projectCookie, sidebarCookie } from '@/cookies.server'
+import { getAllProjects, getProjectTeamMembers } from '@/lib/projects'
 
 export const defaultProjectLoader = async (data: LoaderFunctionArgs) => {
   const headers = data.request.headers.get('Cookie')
+  const token = await authCookie.parse(headers)
   const project = await projectCookie.parse(headers)
-  if (!project) return null
+  if (!project) {
+    const projects = await getAllProjects(token)
+    if (!projects?.length) return null
+    const firstProjectSlug = projects?.[0]?.slug
+    if (projects?.length)
+      return redirect(`/${firstProjectSlug}`, {
+        headers: [
+          ['Set-Cookie', await projectCookie.serialize(firstProjectSlug)],
+          ['Set-Cookie', await sidebarCookie.serialize(true)],
+        ],
+      })
+  }
   return redirect(`/${project}`)
 }
 
